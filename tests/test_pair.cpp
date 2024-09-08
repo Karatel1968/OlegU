7f4993a7b23c480699df1a1cebccd258881ef5da
Oleg Urin <olegurin76@mail.ru>
2024-09-09 01:00:02 +0300

    добавил ещё один тест для конструктора

--- tests/test_pair.cpp
+++ tests/test_pair.cpp
@@ -1,9 +1,17 @@
 #include <gtest.h>
 #include "../lib_pair/pair.h"
 
-// Test the default constructor
-TEST(TPairTest, DefaultConstructor) {
+#define EPSILON 0.000001
+/*TEST(TPairTest, DefaultConstructor) {
     TPair<int, int> pair;
-    EXPECT_EQ(pair.first(), 0);  // Expect default initialization to be 0 for integers
+    EXPECT_EQ(pair.first(), 0);  
     EXPECT_EQ(pair.second(), 0);
+}*/
+
+TEST(TPairTest, DefaultConstructor) {
+    TPair<int, int> pair1(1, 1);
+    TPair<double, int> pair2(1.5, 1);
+    TPair<double, char> pair3(1.5, 'ads');
+
+    ASSERT_NO_THROW(pair1, pair2, pair3);
 }
