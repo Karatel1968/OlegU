3f41b390125c45c5630618ff51fb15690bf37a43
Oleg Urin <olegurin76@mail.ru>
2024-09-28 20:35:29 +0300

    TQueue: Написал тесты для базового конструктора. Реализовал необходимые для этого функции.

--- lib_queue/TQueue.h
+++ lib_queue/TQueue.h
@@ -14,24 +14,24 @@ class TQueue {
 	T* _data;
 	size_t _size;
 	size_t _front;
+	size_t _back;
+	size_t _count;
+
 
 public:
 
 	TQueue(size_t size = 20);
 	~TQueue();
 	void push(T val);
-	size_t front();
-	size_t back();
+	inline T front() const;
 	void swap(TQueue& queue);
-
 	void pop();
 	inline bool isEmpty() const noexcept;
 	inline bool isFull() const noexcept;
-	inline T top() const;
 };
 
 template<class T>
-TQueue<T>::TQueue(size_t size) : _size(size), _front(-1) {
+TQueue<T>::TQueue(size_t size) : _size(size), _front(-1), _back(-1), _count(0) {
 	_data = new T[size];
 }
 
@@ -42,12 +42,30 @@ TQueue<T>::~TQueue() {
 }
 
 template<class T>
+inline bool TQueue<T>::isEmpty() const noexcept {
+	return _count == 0;
+}
+
+template<class T>
+inline bool TQueue<T>::isFull() const noexcept {
+	return _count == _size;
+}
+
+template<class T>
+inline T TQueue<T>::front() const {
+	if (isEmpty()) {
+		throw std::logic_error("TQueue is empty");
+	}
+	return _data[_front];
+}
+
+/*template<class T>
 void TQueue<T>::push(T val) {
 	if (isFull()) {
 		throw std::logic_error("Stack is full");
 	}
 	_top += 1;
 	_data[_top] = val;
-}
+}*/
 
 #endif  // LIB_STACH_STACK_H_
--- tests/test_queue.cpp
+++ tests/test_queue.cpp
@@ -1,7 +1,15 @@
 // Copyright 2024 Urin Oleg
 
 #include <gtest.h>
-#include "../lib_stack/TStack.h"
+#include "../lib_queue/TQueue.h"
 
 
 #define EPSILON 0.000001
+
+TEST(TQueueTest, BaseConstruckttorTest) {
+	TQueue<int> queue(10);
+
+	EXPECT_TRUE(queue.isEmpty());
+	EXPECT_THROW(queue.front(), std::logic_error);
+	EXPECT_FALSE(queue.isFull());
+}
