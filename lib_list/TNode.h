c569bcf08d40040f88c24b3189de8664c770c854
Oleg Urin <olegurin76@mail.ru>
2024-09-30 13:26:44 +0300

    TNode: Реализовал функции.

--- lib_list/TNode.h
+++ lib_list/TNode.h
@@ -23,6 +23,42 @@ public:
 	T value();
 };
 
+template<class T>
+TNode<T>& TNode<T>::operator=(const TNode<T>& node) {
+	if (this != node) {
+		_value = node._value;
+		pnext = node.pnext;
+	}
+
+	return *this;
+}
+
+template<class T>
+TNode<T>* TNode<T>::next() {
+	return pnext;
+}
+
+template<class T>
+TNode<T>::TNode(T val, TNode<T>* node) {
+	_value = val;
+	pnext = node;
+}
+
+template<class T>
+T value() {
+	return _value;
+}
+
+template<class T>
+TNode<T>::TNode(const TNode<T>& node) {
+	_value = node._value;
+	_pnext = node._pnext;
+}
+
+template<class T>
+bool TNode<T>::operator==(TNode<T>* node) {
+	return _value == node->_value;
+}
 
 
 #endif  // LIB_STACH_STACK_H_
