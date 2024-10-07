c5e4406663ecfb61175e7e01a346bd00f8d116ce
Oleg Urin <olegurin76@mail.ru>
2024-10-07 18:51:09 +0300

    DMassive: добавил erase.

--- lib_vector/TVector.h
+++ lib_vector/TVector.h
@@ -62,5 +62,120 @@ TVector<T>::TVector(size_t n, T value) : _data(n, value) _start_index(0) {}
 template <typename T>
 TVector<T>::~TVector() {}
 
+template <typename T>
+bool TVector<T>::empty() const noexcept {
+    return _data.empty();
+}
 
+template <typename T>
+size_t TVector<T>::size() const {
+    return _data.size();
+}
+
+template <typename T>
+size_t TVector<T>::capacity() const {
+    return _data.capacity();
+}
+
+template <typename T>
+void TVector<T>::clear() {
+    _start_index = 0;
+    _data.clear();
+}
+
+template <typename T>
+void TVector<T>::reserve(size_t n) {
+    _data.reserve(n);
+}
+
+template <typename T>
+void TVector<T>::resize(size_t n, T value) {
+    _data.resize(n, value);
+}
+
+template <typename T>
+void TVector<T>::push_back(T value) {
+    _data.push_back(value);
+}
+
+template <typename T>
+void TVector<T>::pop_back() {
+    _data.pop_back();
+}
+
+template <typename T>
+void TVector<T>::push_front(T value) {
+    _data.push_front(value);
+    _start_index = _data[0];
+}
+
+template <typename T>
+void TVector<T>::pop_front() {
+    _data.pop_front();
+    _start_index = _data[0];
+}
+
+template <typename T>
+T& TVector<T>::operator[](size_t index) {
+    if (index >= _data.size()) {
+        throw std::out_of_range("Index out of range");
+    }
+    return const_cast<T&>(_data.data()[index]);
+}
+
+template <typename T>
+const T& TVector<T>::operator[](size_t index) const {
+    if (index >= _data.size()) {
+        throw std::out_of_range("Index out of range");
+    }
+    return _data.data()[index];
+}
+
+template <typename T>
+void TVector<T>::insert(size_t pos, T value) {
+    _data.insert(value, pos);
+    if (pos == 0) {
+        _start_index = _data[0];
+    }
+}
+
+template <typename T>
+void TVector<T>::erase(size_t pos, size_t n) {
+    _data.erase(pos, n);
+    if (pos == 0) {
+        _start_index = _data[0];
+    }
+}
+
+template <typename T>
+void TVector<T>::remove_all(T value) {
+    _data.remove_all(value);
+    _start_index = 0;
+}
+
+template <typename T>
+void TVector<T>::remove_first(T value) {
+    _data.remove_first(value);
+    _start_index = _data[0];
+}
+
+template <typename T>
+void TVector<T>::remove_last(T value) {
+    _data.remove_last(value);
+}
+
+template <typename T>
+size_t TVector<T>::find_first(T value) {
+    return _data.find_first(value);
+}
+
+template <typename T>
+size_t TVector<T>::find_last(T value) {
+    return _data.find_last(value);
+}
+
+template <typename T>
+void TVector<T>::print() const noexcept {
+    _data.print();
+}
 #endif  // LIB_TVector
