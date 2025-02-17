6d737dfc575fe1755f8348a99b81b60e0bb112f1
Oleg Urin <olegurin76@mail.ru>
2025-02-17 10:46:58 +0300

    реализовал insert

--- lib_table/Table.h
+++ lib_table/Table.h
@@ -16,15 +16,31 @@ class Table {
 	Table(Table const&);
 	Table(TKey, TVal);
 	Table();
-	virtual TKey insert(TVal value);
-	virtual void insert(TKey key, TVal value);
-	virtual void erase(TKey key);
-	virtual TVal& find(TKey key);
+	virtual TKey insert(TVal value) = 0;
+	virtual void insert(TKey key, TVal value)=0;
+	virtual void erase(TKey key)=0;
+	virtual TVal& find(TKey key)=0;
 };
 
-template <class TKey, class TVal>
-void Table<TKey, TVal>::insert(TKey key, TVal value) {
-	TPair<TKey, TVal> new_row(key, val);
+//template <class TKey, class TVal>
+//void Table<TKey, TVal>::insert(TKey key, TVal value) {
+//	TPair<TKey, TVal> new_row(key, val);
+//}
+
+template<class TKey, class TVal>
+class UnsortedTable : Table<TKey, TVal> {
+	TList<TPair<TKey, TVal>> _data;
+	size_t _size;
+public:
+	TKey insert(TVal value);
+};
+
+template<class TKey, class TVal>
+TKey UnsortedTable<TKey, TVal>::insert(TVal value) {
+	TPair<TKey, TVal> new_row(generate_key, val);
+	_data.push_back(new_row);
+	_size++;
+	reurn new_key;
 }
 
 
