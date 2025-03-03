79e82264836f5f0a3a042cc28b6907ae6e8b96e8
Oleg Urin <olegurin76@mail.ru>
2025-03-03 21:20:22 +0300

    написал тест для функции слияния списков, исправил функцию

--- lib_algorithms/algorithms.cpp
+++ lib_algorithms/algorithms.cpp
@@ -52,7 +52,7 @@ void listMerge(const TList<int>& list1, const TList<int>& list2) {
 	TNode<int>* cur1 = list1.getHead();
 	TNode<int>* cur2 = list2.getHead();
 	TList<int> list3;
-	while ((cur1->next() != nullptr) && (cur2->next() != nullptr)) {
+	while ((cur1 != nullptr) && (cur2 != nullptr)) {
 		if (cur1->value() >= cur2->value()) {
 			list3.push_back(cur2->value());
 			cur2 = cur2->next();
@@ -63,22 +63,22 @@ void listMerge(const TList<int>& list1, const TList<int>& list2) {
 		}
 	}
 
-	if (cur1->next() == nullptr) {
+	
 		while (cur2 != nullptr) {
 			list3.push_back(cur2->value());
 			cur2 = cur2->next();
 		}
-	}
-	else if (cur2->next() == nullptr) {
+	
+	
 		while (cur1 != nullptr) {
 			list3.push_back(cur1->value());
 			cur1 = cur1->next();
 		}
-	}
+	
 
 	TNode<int>* cur = list3.getHead();
 	while (cur != nullptr) {
-		std::cout << cur->value();
+		std::cout << cur->value() << " ";
 		cur = cur->next();
 	}
 }
--- tests/test_algorithms.cpp
+++ tests/test_algorithms.cpp
@@ -1,11 +1,16 @@
 // Copyright 2024 Urin Oleg
 
 #include <gtest.h>
+#include <iostream>
+#include <sstream>
+#include <functional>
 #include "../lib_algorithms/algorithms.cpp"
 
 
 #define EPSILON 0.000001
 
+//#define CYCLETEST
+#ifdef CYCLETEST
 // Метод черепахи:START
 TEST(AlgorithmsTest, TurtleTest) {
 	TList<int> list;
@@ -60,4 +65,35 @@ TEST(AlgorithmsTest, ReverseCycleTest) {
 	EXPECT_FALSE(isCircle_reverse(list));
 }
 // Метод разворота:END
+#endif
+
+class AlgorithmsTest : public ::testing::Test {
+protected:
+	void captureOutput(std::function<void()> func, std::string& output) {
+		std::stringstream buffer;
+		std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+		func();
+		std::cout.rdbuf(old);
+		output = buffer.str();
+	}
+};
+
+TEST_F(AlgorithmsTest, ListsMerge) {
+	TList<int> list1;
+	list1.push_back(4);
+	list1.push_back(5);
+	list1.push_back(6);
+	TList<int> list2;
+	list2.push_back(1);
+	list2.push_back(2);
+	list2.push_back(3);
+	list2.push_back(4);
+
+	std::string output;
+	captureOutput([&]() { listMerge(list1, list2); }, output);
+
+	EXPECT_EQ(output, "1 2 3 4 4 5 6 ");
+
+}
+
 
