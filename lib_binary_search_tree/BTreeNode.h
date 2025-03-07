bee941c8a5d6b49a745c6359620e669feaa86df7
Oleg Urin <olegurin76@mail.ru>
2025-03-07 16:21:34 +0300

    написал некоторые функции

--- lib_binary_search_tree/BTreeNode.h
+++ lib_binary_search_tree/BTreeNode.h
@@ -12,9 +12,34 @@
 
 template<class T>
 class TBTreeNode {
-	T value;
-	TBTreeNode<T>* right;
-	TBTreeNode<T>* left;
+	T _value;
+	TBTreeNode<T>* _right;
+	TBTreeNode<T>* _left;
+public:
+	TBTreeNode<T>* right();
+	TBTreeNode<T>* left();
+	T& value();
+	const TBTreeNode* getRight() const {
+		return _right;
+	}
+	const TBTreeNode* getLeft() const {
+		return _left;
+	}
 };
+
+template<class T>
+TBTreeNode<T>* TBTreeNode<T>::right() {
+	return _right;
+}
+
+template<class T>
+TBTreeNode<T>* TBTreeNode<T>::left() {
+	return _left;
+}
+
+template<class T>
+T& TBTreeNode<T>::value() {
+	return _value;
+}
 
 #endif  // LIB_BIN_SEARCH_TREE_
