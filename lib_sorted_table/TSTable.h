cc679835c945710d31870d19671fd3af6bc93a8c
Oleg Urin <olegurin76@mail.ru>
2025-03-16 21:03:58 +0300

    добавил новую библиотеку

---
+++ lib_sorted_table/TSTable.h
@@ -0,0 +1,17 @@
+// Copyright 2024 Urin Oleg
+
+#ifndef LIB_SORTED_TABLE_
+#define LIB_SORTED_TABLE_
+#pragma once
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <utility>
+#include <type_traits>
+#include <utility>
+
+#include "../lib_table/Table.h"
+#include "../lib_dmassive/dmassive.h"
+#include "../lib_pair/pair.h"
+
+#endif //LIB_SORTED_TABLE_
