5045c20bf5a3632cb52316407e508a9a3a654b76
Oleg Urin <olegurin76@mail.ru>
2025-03-16 21:52:24 +0300

    начал писать шаблон отсортированной таблицы

--- lib_sorted_table/TSTable.h
+++ lib_sorted_table/TSTable.h
@@ -14,4 +14,25 @@
 #include "../lib_dmassive/dmassive.h"
 #include "../lib_pair/pair.h"
 
+template<class TKey, class TVal>
+class TSTable : public Table<TKey, TVal> {
+	TArchive<TPair<TKey, TVal>> _data;
+
+public:
+    TSTable() = default;
+    TSTable(const TArchive<TPair<TKey, TVal>>& data);
+    TSTable(const TSTable& tab);
+    ~TSTable() = default;
+
+    TKey insert(TVal value) override;
+    void insert(TKey key, TVal val) override;
+    void erase(TKey key) override;
+    TVal find(TKey key) override;
+    int size() override;
+    TSTable& operator=(const TSTable<TKey, TVal>& tab) noexcept;
+};
+
+
+
+
 #endif //LIB_SORTED_TABLE_
