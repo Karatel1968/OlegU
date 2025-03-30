abb956477c3d83cb051b3c263d12ebfea8891b41
Oleg Urin <olegurin76@mail.ru>
2025-03-30 18:51:52 +0300

    написал функции по аналогии с максимальной кучей

--- lib_min_heap/TMinHeap.h
+++ lib_min_heap/TMinHeap.h
@@ -63,4 +63,102 @@ public:
 	TVal remove_min();
 };
 
+template <class TVal>
+void MinHeap<TVal>::emplace(size_t i, TVal val) {
+	if (i >= _size) {
+		throw std::out_of_range("position is out of range");
+	}
+
+	_data[i] = value;
+
+	if (i > 0 && _data[parent(i)] > _data[i]) {
+		sift_up(i);
+	}
+	else {
+		sift_down(i);
+	}
+}
+
+template <class TVal>
+TVal MinHeap<TVal>::remove_min() {
+	TVal t = _data[0];
+	_data[0] = _data[_size - 1];
+	_size--;
+	sift_down(0);
+	return t;
+}
+
+template <class TVal>
+void MinHeap<TVal>::min_heapify() noexcept {
+	for (int i = _size / 2 - 1; i >= 0; --i) {
+		sift_down(i);
+	}
+}
+
+template <class TVal>
+void MinHeap<TVal>::erase(size_t i) {
+	if (_size == 0) {
+		throw std::logic_error("heap is empty");
+	}
+	_data[i] = _data[_size - 1];
+	_size--;
+	sift_down(i);
+}
+
+template <class TVal>
+void MinHeap<TVal>::sift_down(size_t i) noexcept {
+	size_t j;
+	while (left(i) < _size) {
+		if (_data[left(i)] >= _data[right(i)]) {
+			j = left(i);
+		}
+		else {
+			j = right(i);
+		}
+		if (_data[i] >= _data[j]) {
+			std::swap(_data[i], _data[j]);
+			i = j;
+		}
+		else {
+			break;
+		}
+	}
+}
+
+template <class TVal>
+void MinHeap<TVal>::sift_up(size_t i) noexcept {
+	while (i > 0 && _data[parent(i)] > _data[i]) {
+		std::swap(_data[parent(i)], _data[i]);
+		i = parent(i);
+	}
+}
+
+template <class TVal>
+void MinHeap<TVal>::insert(TVal val) noexcept {
+	if (_size == _capacity) {
+		reserve(_capacity + ((_capacity * 25) / 100));
+	}
+	_data[_size] = val;
+	sift_up(_size);
+	_size++;
+}
+
+template <class TVal>
+void MinHeap<TVal>::reserve(size_t new_capacity) {
+
+	if (new_capacity <= STEP_CAPACITY) {
+		return;
+	}
+
+	TVal* new_data = new TVal[new_capacity];
+
+	for (size_t i = 0; i < _size; ++i) {
+		new_data[i] = _data[i];
+	}
+
+	delete[] _data;
+
+	_data = new_data;
+	_capacity = new_capacity;
+}
 #endif  // LIB_MIN_HEAP_
