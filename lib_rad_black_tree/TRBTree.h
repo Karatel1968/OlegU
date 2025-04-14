927920e8ce57449fa4d33d8ad1686e8d6fd6b91b
Oleg Urin <olegurin76@mail.ru>
2025-04-14 09:46:23 +0300

    добавил новую библиотеку

---
+++ lib_rad_black_tree/TRBTree.h
@@ -0,0 +1,21 @@
+// Copyright 2024 Urin Oleg
+
+#ifndef LIB_RAD_BLACK_TREE_
+#define LIB_RAD_BLACK_TREE_
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <utility>
+#include <type_traits>
+#include <utility>
+#include <random>
+
+template<class T>
+class TRBTree {
+
+};
+
+
+#endif // LIB_RAD_BLACK_TREE_
---
+++ lib_rad_black_tree/TRBTreeNode.h
@@ -0,0 +1,21 @@
+// Copyright 2024 Urin Oleg
+
+#ifndef LIB_RAD_BLACK_TREE_NODE_
+#define LIB_RAD_BLACK_TREE_NODE_
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <utility>
+#include <type_traits>
+#include <utility>
+#include <random>
+
+template<class T>
+class TRBTreeNode {
+
+};
+
+
+#endif // LIB_RAD_BLACK_TREE_NODE_
