09bc689bc23fa724e85462e971dc1599ed6f790f
Oleg Urin <olegurin76@mail.ru>
2025-05-01 15:47:02 +0300

    ...

--- tests/test_avl_tree.cpp
+++ tests/test_avl_tree.cpp
@@ -133,3 +133,23 @@ TEST(AVLTreeTest, EraseNodeWithRighrChildTest) {
     EXPECT_EQ(tree.getHead()->getRight()->getRight()->height(), 1);
     EXPECT_EQ(tree.getHead()->getRight()->getLeft()->value(), 25);
 }
+
+TEST(AVLTreeTest, EraseNodeWithTwoChildrenTest) {
+    TAVLTree<int> tree;
+    tree.insert(30);
+    tree.insert(40);
+    tree.insert(20);
+    tree.insert(10);
+    tree.insert(25);
+    tree.insert(5);
+    tree.insert(50);
+    tree.erase(30);
+    EXPECT_EQ(tree.getHead()->height(), 3);
+    EXPECT_EQ(tree.getHead()->getLeft()->height(), 2);
+    EXPECT_EQ(tree.getHead()->getLeft()->getLeft()->value(), 5);
+    EXPECT_EQ(tree.getHead()->getRight()->value(), 30);
+    EXPECT_EQ(tree.getHead()->getRight()->height(), 2);
+    EXPECT_EQ(tree.getHead()->getRight()->getRight()->value(), 50);
+    EXPECT_EQ(tree.getHead()->getRight()->getRight()->height(), 1);
+    EXPECT_EQ(tree.getHead()->getRight()->getLeft()->value(), 25);
+}
