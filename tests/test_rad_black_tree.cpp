1909f627a129bfcb05668be2d7c4141693f9a33d
Oleg Urin <olegurin76@mail.ru>
2025-05-12 00:31:44 +0300

    тест на удаление узла с двумя потомками(листами) проходит

--- tests/test_rad_black_tree.cpp
+++ tests/test_rad_black_tree.cpp
@@ -27,3 +27,21 @@ TEST(TRBTreeTest, EraseLeaf) {
     EXPECT_THROW(tree.search(3), std::logic_error);
 
 }
+
+TEST(TRBTreeTest, EraseNodeWithOneChild) {
+    TRBTree<int> tree;
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(18);
+
+    tree.erase(15);
+
+
+    EXPECT_THROW(tree.search(15), std::logic_error);
+    EXPECT_EQ(tree.getHead()->getRight()->value(), 18);
+
+}
